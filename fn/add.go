5c6a8b10f60d728dac0e846669f19a20e106ff2e
Shaurya <shaurya@Shauryas-mac.local>
2024-08-13 11:51:56 +0530

    comments added

--- fn/add.go
+++ fn/add.go
@@ -7,33 +7,33 @@ import (
 )
 
 func Add(s string) (int, error) {
-	sap := ","
+	sap := "," // saperator
 	if strings.Contains(s, "//") {
-		if len(s) < 3 {
+		if len(s) < 3 { // min len for saperator should be provided
 			return 0, errors.New("minimun length required is 3")
 		}
-		sap = string(s[2])
-		s = strings.Trim(s, "/")
+		sap = string(s[2])       // reinitialize saperator value
+		s = strings.Trim(s, "/") // remove / from string
 	}
-	replacer := strings.NewReplacer("\n", sap)
+	replacer := strings.NewReplacer("\n", sap) // initialzing to replace our \n with sap
 	s = replacer.Replace(s)
 	ar := strings.Split(s, sap)
-	total := 0
+	total := 0 // total till now
 	for _, v := range ar {
 		num := 0
-		if len(v) == 0 {
+		if len(v) == 0 { // if there is not value
 			num = 0
 		} else {
-			cn, er := strconv.Atoi(v)
+			cn, er := strconv.Atoi(v) // convt value to number
 			if er != nil {
 				return 0, er
 			}
-			if cn < 0 {
+			if cn < 0 { // checks for -ve values
 				return 0, errors.New("negetive number not allowed")
 			}
 			num = cn
 		}
-		total += num
+		total += num // add
 	}
 
 	return total, nil
--- main.go
+++ main.go
@@ -6,10 +6,18 @@ import (
 )
 
 func main() {
-	str := "//;\n1;2"
+	str := "//;\n1;2" // simple check case added to check if program is working or not.
 	sm, er := fn.Add(str)
 	if er != nil {
 		panic(er)
 	}
 	fmt.Println(sm)
 }
+
+// Add is the sum function which accepts numbered string saperated
+// all testcases are present in test folder
+// index_test.go
+
+// COMMAND TO RUN TESTCASES:----->
+// cd test
+// go test -bench=.
